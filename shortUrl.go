30a8e08a06ef19fcecaa2c90f98c61417c1872a8
zhouwei <xiaomao361@163.com>
2021-01-08 18:45:33 +0800

    A: add flag to fix bugs

--- shortUrl.go
+++ shortUrl.go
@@ -1,8 +1,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"net/http"
+	"os"
 
 	"zhouwei/shorturl/lib"
 
@@ -13,6 +14,7 @@ import (
 // Client reids
 var Client *redis.Client
 
+// get redis keys value with hash keys
 func getKey(key string) string {
 	val, err := Client.Get(key).Result()
 	if err == redis.Nil {
@@ -25,6 +27,7 @@ func getKey(key string) string {
 	return val
 }
 
+// set redis keys
 func setKey(key string, val string) {
 	err := Client.Set(key, val, 0).Err()
 	if err != nil {
@@ -34,18 +37,56 @@ func setKey(key string, val string) {
 	}
 }
 
-func init() {
-	Client = redis.NewClient(&redis.Options{
-		Addr:     "test.yjwh.shop:6379",
-		Password: "123456",
-
-		DB: 13,
-	})
-}
+// init func can not take params
+// func init() {
+// 	Client = redis.NewClient(&redis.Options{
+// 		Addr:     "redis:6379",
+// 		Password: "heals@2020.",
+// 		DB:       13,
+// 	})
+// }
 
 func main() {
+
+	// static vars
+	var redisHost string
+	var redisPassWord string
+	var redisPort string
+	var baseURL string
+	var port string
+
+	/// flags
+	flag.StringVar(&redisHost, "h", "127.0.0.1", "Redis host, default is 127.0.0.1")
+	flag.StringVar(&redisPassWord, "pw", "", "Redis password, default is null")
+	flag.StringVar(&redisPort, "port", "6379", "Redis port, default is 6370")
+	flag.StringVar(&baseURL, "url", "", "base url")
+	flag.StringVar(&port, "p", "3070", "server web port")
+	flag.Parse()
+
+	// check input vars
+	if baseURL == "" {
+		fmt.Println("you need input a baseURL")
+		fmt.Println("usage ./shortUrl --help check input params")
+		os.Exit(0)
+	}
+
+	// print the input params
+	fmt.Println("redis host:", redisHost)
+	fmt.Println("redis password:", redisPassWord)
+	fmt.Println("redis port:", redisPassWord)
+	fmt.Println("base url:", baseURL)
+	fmt.Println("server port:", port)
+
+	Client = redis.NewClient(&redis.Options{
+		Addr:     redisHost + ":" + redisPort,
+		Password: redisPassWord,
+		DB:       13,
+	})
+
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
+
+	// health check
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
@@ -56,9 +97,10 @@ func main() {
 		url := c.Request.RequestURI
 		if len(url) == 7 {
 			val := getKey(url[1:7])
-			c.Redirect(http.StatusMovedPermanently, val)
+			c.Redirect(301, val)
 		} else {
 			fmt.Println("err")
+
 		}
 
 	})
@@ -67,10 +109,11 @@ func main() {
 		url := c.Query("url")
 		result, _ := lib.Transform(url)
 
-		c.JSON(200, "https://d.yjwh.shop/"+result[0])
+		c.JSON(200, "https://"+baseURL+"/"+result[0])
 		setKey(result[0], url)
-		fmt.Println("shortUrl is https://d.yjwh.shop/", url)
+		fmt.Println("shortUrl is", "https://"+baseURL+"/"+url)
 	})
 
-	r.Run(":3070") // listen and serve on 0.0.0.0:8080
+	fmt.Println("server start on port ", port)
+	r.Run(":" + port) // default listen and serve on 0.0.0.0:3070
 }
