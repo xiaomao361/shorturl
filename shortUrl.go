601625a6a68e05717343b59505b16b25b2b2756f
zhouwei <xiaomao361@163.com>
2021-01-11 09:56:33 +0800

    test

--- shortUrl.go
+++ shortUrl.go
@@ -12,32 +12,6 @@ import (
 	"github.com/go-redis/redis"
 )
 
-// Client reids
-var Client *redis.Client
-
-// get redis keys value with hash keys
-func getKey(key string) string {
-	val, err := Client.Get(key).Result()
-	if err == redis.Nil {
-		fmt.Println("keys doe not exist")
-	} else if err != nil {
-		panic(err)
-	} else {
-		fmt.Println(key, val)
-	}
-	return val
-}
-
-// set redis keys
-func setKey(key string, val string) {
-	err := Client.Set(key, val, 0).Err()
-	if err != nil {
-		panic(err)
-	} else {
-		fmt.Println("key set success")
-	}
-}
-
 func main() {
 
 	// static vars
